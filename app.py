3797021d6e25e3bf3b82242669afc0d05e4eb505
FlowerSuc <147378087+FlowerSuc@users.noreply.github.com>
2025-05-09 23:16:38 +0800

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, jsonify, send_from_directory
 from werkzeug.utils import secure_filename
 import os
+from flask_cors import CORS
 from PIL import Image
 import torch
 from options import TrainOptions
 import RPTC
 import process
 import time
+import io
 
 model_path = "RPTC.pth"
 opt = TrainOptions().parse(print_option=False)
@@ -17,8 +19,7 @@ model.load_state_dict({k.replace('module.', ''): v for k, v in state_dict['netC'
 model.eval()
 
 
-def predict(img_path, opt):
-    img = Image.open(img_path).convert('RGB')
+def predict(img, opt):
     input_tensor = process.processing_RPTC(img, opt).unsqueeze(0)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
@@ -26,24 +27,13 @@ def predict(img_path, opt):
         output = model(input_tensor)
         result = torch.sigmoid(output).item()
 
-    print(img_path)
     print(f'AI-generated probability: {round(result*100, 3)}%')
     prob = round(result*100, 3)
     return prob
 
 
 app = Flask(__name__, static_folder='static')
-
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+CORS(app)
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -51,41 +41,21 @@ def serve_frontend():
     return send_from_directory(app.static_folder, 'ai_detector_spa.html')
 
 
-@app.route('/upload', methods=['POST', 'GET'])
-def upload_file():
-    if 'image' not in request.files:
-        return jsonify({'error': 'No file part in request'}), 400
-
-    file = request.files['image']
-
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
-
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(filepath)
-        # predict(filepath, opt)
-        return jsonify({'message': 'File uploaded successfully', 'filepath': filepath}), 200
-    else:
-        return jsonify({'error': 'Invalid file type'}), 400
-
-
 @app.route('/detect', methods=['POST', 'GET'])
 def detect_image():
-    data = request.get_json()
-    image_path = data.get('image_path')
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image upload'}), 400
 
-    if not image_path or not os.path.exists(image_path):
-        return jsonify({'error': 'Invalid or missing image path'}), 400
+    image_file = request.files['image']
 
     try:
         # time.sleep(1.5)
-        prob = predict(image_path, opt)
-        return jsonify({'probability': prob})
+        image = Image.open(io.BytesIO(image_file.read())).convert('RGB')
+        prob = predict(image, opt)
+        return jsonify({'probability': prob}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run(host="0.0.0.0", port=5000)
